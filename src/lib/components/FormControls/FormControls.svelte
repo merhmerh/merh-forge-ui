9479650177a85da506e587f2bddf0eb63d1a9d17
Arvin How <arvinhow95@gmail.com>
2024-04-18 15:12:15 +0800

    add customizable text for forms controls

--- src/lib/components/FormControls/FormControls.svelte
+++ src/lib/components/FormControls/FormControls.svelte
@@ -2,6 +2,8 @@
 import { createEventDispatcher } from "svelte";
 
 export let reverse;
+export let confirmText = "Save";
+export let cancelText = "Cancel";
 let confirmButton, cancelButton;
 const dispatch = createEventDispatcher();
 
@@ -15,8 +17,8 @@ function cancel() {
 </script>
 
 <div class="formControls" class:reverse>
-    <button bind:this={confirmButton} class="" on:click={confirm}>Save</button>
-    <button bind:this={cancelButton} class="secondary" on:click={cancel}>Cancel</button>
+    <button bind:this={confirmButton} class="" on:click={confirm}>{confirmText}</button>
+    <button bind:this={cancelButton} class="secondary" on:click={cancel}>{cancelText}</button>
 </div>
 
 <style lang="scss">
